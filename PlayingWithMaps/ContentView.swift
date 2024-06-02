8b75dda2e95857503c138a977274edc037fd067e
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2024-06-02 11:02:00 -0700

    Initial Commit

---
+++ PlayingWithMaps/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlayingWithMaps
+//
+//  Created by Jennifer Lee on 6/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlayingWithMaps/PlayingWithMapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayingWithMapsApp.swift
+//  PlayingWithMaps
+//
+//  Created by Jennifer Lee on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct PlayingWithMapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
