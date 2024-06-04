640bf2b26c012723af1efa1eca798e336e183d86
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2024-06-04 12:35:33 -0700

    initial commit

---
+++ PlayingWithMaps/ItemInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemInfoView.swift
+//  PlayingWithMaps
+//
+//  Created by Jennifer Lee on 6/4/24.
+//
+
+import SwiftUI
+
+struct ItemInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemInfoView()
+}
---
+++ PlayingWithMaps/LAButtons.swift
@@ -0,0 +1,18 @@
+//
+//  LAButtons.swift
+//  PlayingWithMaps
+//
+//  Created by Jennifer Lee on 6/2/24.
+//
+
+import SwiftUI
+
+struct LAButtons: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LAButtons()
+}
